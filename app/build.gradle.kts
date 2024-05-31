0f285d80812f0008b31f61202aeab0ad74d8870f
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-05-31 23:19:36 +0530

    Added Dependencies and colors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -26,6 +27,11 @@ android {
             )
         }
     }
+
+    viewBinding {
+        enable = true
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -36,7 +42,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -45,32 +50,31 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-
     implementation(libs.fuel)
     implementation(libs.androidx.core.ktx.v1120)
 
-    //FireBase Dependencies
+    // Firebase Dependencies
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
-    implementation (libs.firebase.ui.database)
+    implementation(libs.firebase.ui.database)
     implementation(libs.firebase.messaging)
     implementation(libs.firebase.database.v2030)
+    implementation(libs.firebase.core)
+    implementation(libs.firebase.firestore.v2480)
 
-
-    //Google Services
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation (libs.glide)
+    implementation(libs.glide)
     implementation(libs.shadowlayout)
-    implementation (libs.circleimageview)
+    implementation(libs.circleimageview)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.coroutines.core)
-    implementation (libs.converter.gson)
-    implementation (libs.retrofit)
-    implementation (libs.shimmer)
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
+    implementation(libs.timber)
+    implementation(libs.shimmer)
 
     implementation(libs.generativeai)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
