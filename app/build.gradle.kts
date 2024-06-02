adbc2670b19eefe99579dea7a158980150eac853
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-06-02 22:07:21 +0530

    Added Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -27,11 +26,9 @@ android {
             )
         }
     }
-
     viewBinding {
         enable = true
     }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -42,39 +39,42 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.runtime.livedata)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
     implementation(libs.fuel)
     implementation(libs.androidx.core.ktx.v1120)
 
-    // Firebase Dependencies
+    //FireBase Dependencies
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
-    implementation(libs.firebase.ui.database)
+    implementation (libs.firebase.ui.database)
     implementation(libs.firebase.messaging)
     implementation(libs.firebase.database.v2030)
     implementation(libs.firebase.core)
     implementation(libs.firebase.firestore.v2480)
-
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.glide)
+    implementation (libs.glide)
     implementation(libs.shadowlayout)
-    implementation(libs.circleimageview)
+    implementation (libs.circleimageview)
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.converter.gson)
-    implementation(libs.retrofit)
+    implementation (libs.converter.gson)
+    implementation (libs.retrofit)
+    implementation (libs.shimmer)
     implementation(libs.timber)
-    implementation(libs.shimmer)
 
     implementation(libs.generativeai)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
@@ -82,5 +82,6 @@ dependencies {
     implementation(libs.androidx.material)
     implementation(libs.androidx.navigation.fragment.ktx.v276)
     implementation(libs.androidx.navigation.ui.ktx.v276)
+    implementation (libs.play.services.maps)
     implementation(libs.play.services.location)
 }
