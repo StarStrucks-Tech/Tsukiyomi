8d81a30826faa1cb8a459f6a275f380c2c0cfe01
Saurabh Kumar <saurabhk.ug22.ec@nitp.ac.in>
2024-06-06 21:37:01 +0530

    added repository

--- app/src/main/java/com/starstruckstech/pgpodandroid/repository/DataRepository.kt
+++ app/src/main/java/com/starstruckstech/pgpodandroid/repository/DataRepository.kt
@@ -4,18 +4,15 @@ import com.starstruckstech.pgpodandroid.network_helper.NetworkHelper
 /**
  * It acts as a mediator between the network layer and the rest of the application.
  */
-class DataRepository (private val networkHelper: NetworkHelper){
-    /**
-     * Constructs a DataRepository with the provided NetworkHelper instance.
-     * @param networkHelper the NetworkHelper instance used to perform network operations
-     */
-    fun getPosts(callback: (List<Post>?)->Unit){
-        networkHelper.getPosts{results->
-                callback(results)
-            }
-        }
 
+class DataRepository(private val networkHelper: NetworkHelper) {
+    fun getPosts(callback: (List<Post>?) -> Unit) {
+        networkHelper.getPosts { result ->
+            callback(result)
+        }
+    }
 }
+
 
 data class Post(
     val userId: Int,
