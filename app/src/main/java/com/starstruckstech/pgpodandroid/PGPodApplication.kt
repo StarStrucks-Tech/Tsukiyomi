4719f5870a2712870161c4e96dac8256c637d3e1
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-06-06 21:09:05 +0530

    Added JavaDocs to PGPodApplication File

--- app/src/main/java/com/starstruckstech/pgpodandroid/PGPodApplication.kt
+++ app/src/main/java/com/starstruckstech/pgpodandroid/PGPodApplication.kt
@@ -1,8 +1,13 @@
-package com.starstruckstech.pgpodandroid.central_logger
+package com.starstruckstech.pgpodandroid
 
 import android.app.Application
 import timber.log.Timber
 
+/**
+ * This class is used to initialize the Timber library for logging
+ * @constructor Creates an instance of PGPodApplication
+ * @return Nothing
+ */
 class PGPodApplication : Application() {
     override fun onCreate() {
         super.onCreate()
